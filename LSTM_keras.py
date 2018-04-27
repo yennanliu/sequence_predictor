7b131682c70ee5fa40b7927b84bda3effc0ca62d
yennanliue <f339339@gmail.com>
2018-04-27 01:33:32 +0100

    add 5 hidden layers LSTM and fix/add comments

--- LSTM_keras.py
+++ LSTM_keras.py
@@ -53,8 +53,6 @@ def get_train_data(df):
 	return dataset
 
 
-#------------------------
-# DL 
 
 # convert an array of values into a dataset matrix
 def create_dataset(dataset, look_back=1):
@@ -64,6 +62,10 @@ def create_dataset(dataset, look_back=1):
 		dataX.append(a)
 		dataY.append(dataset[i + look_back, 0])
 	return np.array(dataX), np.array(dataY)
+
+
+#------------------------
+# DL 
 
 
 def one_input_LSTM_model_1(dataset):
@@ -83,6 +85,7 @@ def one_input_LSTM_model_1(dataset):
 	testX = np.reshape(testX, (testX.shape[0], 1, testX.shape[1]))
 	# create and fit the LSTM network
 	model = Sequential()
+	# expected input data shape: (batch_size, input_shape=(timesteps, data_dim))
 	model.add(LSTM(4, input_shape=(1, look_back)))
 	model.add(Dense(1))
 	model.compile(loss='mean_squared_error', optimizer='adam')
@@ -133,6 +136,13 @@ def one_input_LSTM_model_2(dataset):
 	testX = np.reshape(testX, (testX.shape[0], 1, testX.shape[1]))
 	# create and fit the LSTM network
 	model = Sequential()
+	"""
+	https://github.com/keras-team/keras/issues/160
+	A LSTM layer, as per the docs, will return the last vector by default rather than the entire sequence. 
+	In order to return the entire sequence (which is necessary to be able to stack LSTM), 
+	use the constructor argument return_sequences=True.
+	"""
+	# expected input data shape: (batch_size, input_shape=(timesteps, data_dim))
 	model.add(LSTM(4, return_sequences=True,input_shape=(1, look_back)))
 	model.add(LSTM(4, input_shape=(1, look_back)))
 	model.add(Dense(1))
@@ -162,6 +172,65 @@ def one_input_LSTM_model_2(dataset):
 	print (model.summary())
 	return  dataset,trainPredict,testPredict,trainPredictPlot,testPredictPlot
 
+
+def one_input_LSTM_model_3(dataset):
+	# normalize the dataset
+	scaler = MinMaxScaler(feature_range=(0, 1))
+	dataset = scaler.fit_transform(dataset)
+	# split into train and test sets
+	train_size = int(len(dataset) * 0.67)
+	test_size = len(dataset) - train_size
+	train, test = dataset[0:train_size,:], dataset[train_size:len(dataset),:]
+	# reshape into X=t and Y=t+1
+	look_back = 1
+	trainX, trainY = create_dataset(train, look_back)
+	testX, testY = create_dataset(test, look_back)
+	# reshape input to be [samples, time steps, features]
+	trainX = np.reshape(trainX, (trainX.shape[0], 1, trainX.shape[1]))
+	testX = np.reshape(testX, (testX.shape[0], 1, testX.shape[1]))
+	# create and fit the LSTM network
+	model = Sequential()
+	"""
+	https://github.com/keras-team/keras/issues/3522
+	A LSTM layer, as per the docs, will return the last vector by default rather than the entire sequence. 
+	In order to return the entire sequence (which is necessary to be able to stack LSTM), 
+	use the constructor argument return_sequences=True.
+	"""
+	# expected input data shape: (batch_size, input_shape=(timesteps, data_dim))
+	model.add(LSTM(4, return_sequences=True,input_shape=(1, look_back)))
+	# add 5 stack hidden layers 
+	for epoch in range(5):
+		model.add(LSTM(4, return_sequences=True,input_shape=(1, look_back)))
+	model.add(LSTM(4, input_shape=(1, look_back)))
+	model.add(Dense(1))
+	model.compile(loss='mean_squared_error', optimizer='adam')
+	model.fit(trainX, trainY, epochs=20, batch_size=1, verbose=2)
+	# make predictions
+	trainPredict = model.predict(trainX)
+	testPredict = model.predict(testX)
+	# invert predictions
+	trainPredict = scaler.inverse_transform(trainPredict)
+	trainY = scaler.inverse_transform([trainY])
+	testPredict = scaler.inverse_transform(testPredict)
+	testY = scaler.inverse_transform([testY])
+	# calculate root mean squared error
+	trainScore = math.sqrt(mean_squared_error(trainY[0], trainPredict[:,0]))
+	print('Train Score: %.2f RMSE' % (trainScore))
+	testScore = math.sqrt(mean_squared_error(testY[0], testPredict[:,0]))
+	print('Test Score: %.2f RMSE' % (testScore))
+	# shift train predictions for plotting
+	trainPredictPlot = np.empty_like(dataset)
+	trainPredictPlot[:, :] = np.nan
+	trainPredictPlot[look_back:len(trainPredict)+look_back, :] = trainPredict
+	# shift test predictions for plotting
+	testPredictPlot = np.empty_like(dataset)
+	testPredictPlot[:, :] = np.nan
+	testPredictPlot[len(trainPredict)+(look_back*2)+1:len(dataset)-1, :] = testPredict
+	print (model.summary())
+	return  dataset,trainPredict,testPredict,trainPredictPlot,testPredictPlot
+
+
+
 #---------------------------------------
 
 
@@ -171,7 +240,7 @@ if __name__ == '__main__':
 	df_FB_ = col_fix(df_FB)
 	dataset = get_train_data(df_FB_)
 	#dataset,trainPredict,testPredict,trainPredictPlot,testPredictPlot = one_input_LSTM_model_1(dataset)
-	dataset,trainPredict,testPredict,trainPredictPlot,testPredictPlot = one_input_LSTM_model_2(dataset)
+	dataset,trainPredict,testPredict,trainPredictPlot,testPredictPlot = one_input_LSTM_model_3(dataset)
 	# plot 
 	#plt.plot(trainPredictPlot)
 	#plt.plot(testPredictPlot)
