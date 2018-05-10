5d861a2b58ef22734ddb4de6988a448d4d8ae66e
yennanliue <f339339@gmail.com>
2018-05-10 19:43:01 +0100

    add V2 model runner with import pre-defined model architecture

--- model_LSTM_keras.py
+++ model_LSTM_keras.py
@@ -8,7 +8,7 @@ import math
 #%pylab inline
 
 
-# ML
+# DL
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.metrics import mean_squared_error
 from keras.models import Sequential
@@ -23,8 +23,9 @@ from keras.callbacks import Callback
 #---------------------------------------
 
 
+#########  data preprocess #########
 
-# data prepare 
+
 def get_data(fine_name):
 	print (fine_name)
 	df = pd.read_csv('data/{}.csv'.format(fine_name))
@@ -68,8 +69,7 @@ def create_dataset(dataset, look_back=1):
 
 #########  V1 ARCHITECTURE #########
 
-
-
+# V1 model 
 def one_input_LSTM_model_1(dataset):
 	# normalize the dataset
 	scaler = MinMaxScaler(feature_range=(0, 1))
@@ -241,7 +241,9 @@ def one_input_LSTM_model_3(dataset):
 
 #########  V2 ARCHITECTURE #########
 
-
+# V2 model 
+# credit  
+# https://medium.com/machine-learning-world/neural-networks-for-algorithmic-trading-1-2-correct-time-series-forecasting-backtesting-9776bfd9e589
 
 def dev_model_1():
 	model = Sequential() 
@@ -287,9 +289,54 @@ def dev_model_4():
 
 
 
+# V2 ARCHITECTURE runner 
+
+def V2_model_runner(dataset, model):
+	# normalize the dataset
+	scaler = MinMaxScaler(feature_range=(0, 1))
+	dataset = scaler.fit_transform(dataset)
+	# split into train and test sets
+	train_size = int(len(dataset) * 0.67)
+	test_size = len(dataset) - train_size
+	train, test = dataset[0:train_size,:], dataset[train_size:len(dataset),:]
+	# reshape into X=t and Y=t+1
+	look_back = 1
+	trainX, trainY = create_dataset(train, look_back)
+	testX, testY = create_dataset(test, look_back)
+	# reshape input to be [samples, time steps, features]
+	trainX = np.reshape(trainX, (trainX.shape[0], 1, trainX.shape[1]))
+	testX = np.reshape(testX, (testX.shape[0], 1, testX.shape[1])) 
+	####### import pre-defined model #######
+	print (model) 
+	# make predictions
+	trainPredict = model.predict(trainX)
+	testPredict = model.predict(testX)
+	# invert predictions
+	trainPredict = scaler.inverse_transform(trainPredict)
+	trainY = scaler.inverse_transform([trainY])
+	testPredict = scaler.inverse_transform(testPredict)
+	testY = scaler.inverse_transform([testY])
+	# calculate root mean squared error
+	trainScore = math.sqrt(mean_squared_error(trainY[0], trainPredict[:,0]))
+	print('Train Score: %.2f RMSE' % (trainScore))
+	testScore = math.sqrt(mean_squared_error(testY[0], testPredict[:,0]))
+	print('Test Score: %.2f RMSE' % (testScore))
+	# shift train predictions for plotting
+	trainPredictPlot = np.empty_like(dataset)
+	trainPredictPlot[:, :] = np.nan
+	trainPredictPlot[look_back:len(trainPredict)+look_back, :] = trainPredict
+	# shift test predictions for plotting
+	testPredictPlot = np.empty_like(dataset)
+	testPredictPlot[:, :] = np.nan
+	testPredictPlot[len(trainPredict)+(look_back*2)+1:len(dataset)-1, :] = testPredict
+	print (model.summary())
+	return  dataset,trainPredict,testPredict,trainPredictPlot,testPredictPlot
+
+
+
+
+
 #---------------------------------------
-
-
 
 
 if __name__ == '__main__':
